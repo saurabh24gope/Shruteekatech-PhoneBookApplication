4183fa93ee6756f38459c33d5abf6ae7f6a31d8e
saurabh24gope <saurabhgope@gmail.com>
2022-12-08 00:01:33 +0530

    SP-4 create a springboot project

---
+++ src/main/java/com/shruteekaTech/PhoneBookApp/ShruteeKaTechPhoneBookAppApplication.java
@@ -0,0 +1,13 @@
+package com.shruteekaTech.PhoneBookApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShruteeKaTechPhoneBookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShruteeKaTechPhoneBookAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shruteekaTech/PhoneBookApp/ShruteeKaTechPhoneBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shruteekaTech.PhoneBookApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShruteeKaTechPhoneBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
